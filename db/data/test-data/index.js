5efde27737787441d47e5dc5ba126fc5894b35b5
Simon Green <simonisaacgreen@icloud.com>
2019-09-23 12:11:44 +0100

    Initial commit adding an index.js file into each data directory and an index.js file into the db directory

---
+++ db/data/test-data/index.js
@@ -0,0 +1,4 @@
+exports.topicData = require('./topics.js');
+exports.articleData = require('./articles.js');
+exports.userData = require('./users.js');
+exports.commentData = require('./comments.js');
---
+++ db/index.js
@@ -0,0 +1,11 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+const testData = require('./test-data');
+const developmentData = require('./development-data');
+
+const data = {
+  development: developmentData,
+  test: testData
+};
+
+module.exports = data[ENV];
