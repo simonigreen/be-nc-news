91552d57399bd70d72ea6d8a31fba0b887845cf1
Simon Green <simonisaacgreen@icloud.com>
2019-09-23 14:49:55 +0100

    Creating migrations for all four tables, moving index.js from db directory into db/data

---
+++ db/migrations/20190923123424_create_topics_table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  console.log('creating topics table...');
+  return knex.schema.createTable('topics', topicsTable => {
+    topicsTable
+      .string('slug')
+      .primary()
+      .notNullable();
+    topicsTable.string('description').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  console.log('removing topics table...');
+  return knex.schema.dropTable('topics');
+};
---
+++ db/migrations/20190923125447_create_articles_table.js
@@ -0,0 +1,26 @@
+exports.up = function(knex) {
+  console.log('creating articles table...');
+  return knex.schema.createTable('articles', articlesTable => {
+    articlesTable.increments('article_id').primary();
+    articlesTable.string('title').notNullable();
+    articlesTable.text('body').notNullable();
+    articlesTable
+      .integer('votes')
+      .defaultTo(0)
+      .notNullable();
+    articlesTable
+      .string('slug')
+      .references('topics.slug')
+      .notNullable();
+    articlesTable
+      .string('author')
+      .references('users.username')
+      .notNullable();
+    articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  console.log('removing articles table...');
+  return knex.schema.dropTable('articles');
+};
