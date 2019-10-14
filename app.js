d1974a99eaf69a826edf9ad838fb3f4dcb875180
Simon Green <simonisaacgreen@icloud.com>
2019-10-14 09:26:08 +0100

    adding cors to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const apiRouter = require('./routes/api-router');
 const {
   handle400,
@@ -9,6 +10,7 @@ const {
   handleCustomErrors
 } = require('./errors');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
